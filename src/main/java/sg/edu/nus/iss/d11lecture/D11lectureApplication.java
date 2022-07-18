e4b6bf04f6faea5a436acb0caa0952c56082bf9b
zawan1997 <nawaz.shss@gmail.com>
2022-07-18 10:27:15 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D11lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D11lectureApplication.class, args);
+	}
+
+}
